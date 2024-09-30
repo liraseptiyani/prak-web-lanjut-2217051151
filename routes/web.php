f1e9c0e0a6fa54a271ba2e3c8aef4c4e299437bc
liraseptiyani <liraseptiyani14@gmail.com>
2024-09-30 15:54:01 +0700

    add code

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', [ProfileController::class, 'showProfile']);
+
+Route::get('/user/profile', [UserController::class,'profile']);
+
+Route::get('/user/create', [UserController::class,'create']);
+
+Route::post('/user/store', [UserController::class,'store'])->name('user.store');
